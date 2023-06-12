8fc5a8f0b8ac3b98a976d4103ea0217c671b0dac
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-12 11:52:19 -0300

    estructura server

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-// index.js
-//algo mas
+const server = require("./src/server");
+const db = require("./src/db/db");
+
+const PORT = process.env.PORT || 3001
+
+server.listen(PORT, () => {
+    console.log('Listen on port 3001');
+})
+
+db();
